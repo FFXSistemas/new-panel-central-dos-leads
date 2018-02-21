32b0fdfe6433cade4ae8f17120eea1ec92182da4
Marcos Santiago <santiago@ffxsistemas.com>
2018-02-21 11:28:25 -0300

    added the mailgun method

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -1,27 +1,50 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: FFX Sistemas
- * Date: 20/02/2018
- * Time: 16:02
- */
 
 namespace App\Http\Controllers\API;
 
 
 use App\Http\Controllers\Controller;
+use App\Repositories\RecoveryPasswordRepository;
+use App\Repositories\UsersRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Mailgun\Mailgun;
 
 class UsersController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return array
+     * @throws \Mailgun\Messages\Exceptions\MissingRequiredMIMEParameters
+     * @throws \Throwable
+     */
     public function recoveryEmail(Request $request)
     {
-        var_dump($request->all());
+        $args = $request->all();
+        $user = (new UsersRepository())->findByEmail($args['email']);
+
+        if($user){
+            $mgClient = new Mailgun('key-2dde12b672e70384e927810d92eab93a');
+            $domain = "suporte.centraldosleads.com.br";
+            $token = base64_encode($user->email);
+            $template = view('templates.emails.recovery', ['token' => md5($token)])->render();
+            $recovery = (new RecoveryPasswordRepository())->create([
+                'token' => md5($token),
+                'active' => '1',
+                'user_id' => $user->id
+            ]);
+            $result = $mgClient->sendMessage("$domain",
+                array('from'    => 'Central dos Leads <atendimento@centraldosleads.com.br>',
+                    'to'      => $user->email,
+                    'subject' => 'Solicitação de Reset de senha.',
+                    'html'    => $template));
+            return [
+                'success' => true,
+            ];
+        }
         return [
-            'success' => true,
+            'success' => false,
+            'alert' => 'Usuário não encontrado. Por favor verifique o email e tente novamente.'
         ];
-
     }
-
 }
