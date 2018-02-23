b940fe97a0f92249da52778ff5b711120a70befd
Marcos Santiago <santiago@ffxsistemas.com>
2018-02-23 19:00:30 -0300

    created the method for update schedules

--- app/Http/Controllers/API/BotMailController.php
+++ app/Http/Controllers/API/BotMailController.php
@@ -126,4 +126,60 @@ class BotMailController extends Controller
         $update = (new UpdateSystemRepository())->updateByType('roboc_crivo');
         dd($update);
     }
+
+    /**
+     * @param EmailsRepository $repository
+     */
+    public function getSchedules(EmailsRepository $repository)
+    {
+        $server = new Server(env('BOT_MAIL_SERVER'), 143);
+        $server->setAuthentication(env('BOT_SCHEDULE_LOGIN'), env('BOT_SCHEDULE_PASS'));
+
+        /** @var Message[] $message */
+        $messages = $server->getOrderedMessages(SORTDATE,1, 30   );
+        foreach ($messages as $message){
+            $subject = str_replace("AGENDAMENTO - ", "", $message->getSubject());
+            var_dump($subject);
+
+            if(is_numeric($subject)) {
+                $search = 'Sua solicitação foi atendida com sucesso';
+                $search2 = 'Liberado';
+                $search3 = 'Feito';
+                $found = strpos_recursive($message->getMessageBody(), $search);
+                $found2 = strpos_recursive($message->getMessageBody(), $search2);
+                $found3 = strpos_recursive($message->getMessageBody(), $search3);
+
+                if($found) {
+                    $values = [
+                        "body" => $message->getMessageBody(),
+                        "status" => "APROVADO"
+                    ];
+                    $repository->updateByScheduledOrder($subject, $values );
+                } else if($found2){
+                    $values = [
+                        "body" => $message->getMessageBody(),
+                        "status" => "APROVADO"
+                    ];
+                    $repository->updateByScheduledOrder($subject, $values );
+                }else if($found3){
+                    $values = [
+                        "body" => $message->getMessageBody(),
+                        "status" => "APROVADO"
+                    ];
+                    $repository->updateByScheduledOrder($subject, $values );
+                } else {
+                    $values = [
+                        "body" => $message->getMessageBody(),
+                        "status" => "REPROVADO"
+                    ];
+                    $repository->updateByScheduledOrder($subject, $values );
+                }
+            }
+            echo 'Subject: '.$subject.''.PHP_EOL; echo "<br>";
+            echo 'Body: '.$message->getMessageBody().''. PHP_EOL; echo "<br>";
+        }
+
+        $update = (new UpdateSystemRepository())->updateByType('roboc_schedule');
+        dd($update);
+    }
 }
